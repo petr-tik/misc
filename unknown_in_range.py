947a61f4bd2a0fa0e8a0f78e0dd1d239f25fc077
Petr Tikilyaynen <petr-tik@users.noreply.github.com>
2016-01-27 16:01:46 +0000

    Create unknown_in_range.py
    random interview question David Beck suggested

---
+++ unknown_in_range.py
@@ -0,0 +1,18 @@
+"""
+a Q from David Beck
+Given an unsorted sequence of numbers in the range between x and y (eg 1..100) with just one number missing. 
+Find a O(k) algorithm to calculate the missing value
+"""
+
+def generate_seq(a, b):
+  # a - lower, b - upper bound
+  pass
+  # delete random member
+  
+  
+def find_missing(seq):
+  pass
+  
+  
+
+
