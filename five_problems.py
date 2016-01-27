7e12964a15110122a65e0be4e985835318cbc44e
Petr Tikilyaynen <petr-tik@users.noreply.github.com>
2016-01-27 11:04:01 +0000

    Update five_problems.py

--- five_problems.py
+++ five_problems.py
@@ -7,7 +7,8 @@ https://www.shiftedup.com/2015/05/07/five-programming-problems-every-software-en
 
 The 5 problems
 
-(The following problems are ridiculously simple, but you'd be surprise to discover how many people struggle with them. To the point of not getting anything done at all. Seriously.)
+(The following problems are ridiculously simple, but you'd be surprise to discover how many people struggle with them. 
+To the point of not getting anything done at all. Seriously.)
 
 Problem 1
 
@@ -15,7 +16,8 @@ Write three functions that compute the sum of the numbers in a given list using 
 
 Problem 2
 
-Write a function that combines two lists by alternatingly taking elements. For example: given the two lists [a, b, c] and [1, 2, 3], the function should return [a, 1, b, 2, c, 3].
+Write a function that combines two lists by alternatingly taking elements. 
+Eg: given the two lists [a, b, c] and [1, 2, 3], the function should return [a, 1, b, 2, c, 3].
 
 Problem 3
 
@@ -26,13 +28,16 @@ The first 10 Fibonnaci numbers: 0, 1, 1, 2, 3, 5, 8, 13, 21, and 34.
 
 Problem 4
 
-Write a function that given a list of non negative integers, arranges them such that they form the largest possible number. For example, given [50, 2, 1, 9], the largest formed number is 95021.
+Write a function that given a list of non negative integers, arranges them such that they form the largest possible number. 
+Eg given [50, 2, 1, 9], the largest formed number is 95021.
 
-Update: Apparently this problem got a lot of people talking (although not as much as Problem 5 below.) You can click here to read my solution.
+Update: Apparently this problem got a lot of people talking (although not as much as Problem 5 below.) 
+You can click here to read my solution.
 
 Problem 5
 
-Write a program that outputs all possibilities to put + or - or nothing between the numbers 1, 2, ..., 9 (in this order) such that the result is always 100. For example: 1 + 2 + 34 – 5 + 67 – 8 + 9 = 100.
+Write a program that outputs all possibilities to put + or - or nothing between the numbers 1, 2, ..., 9 (in this order) 
+such that the result is always 100. For example: 1 + 2 + 34 – 5 + 67 – 8 + 9 = 100.
 
 """
 
