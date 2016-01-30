db84fe16c233c51b4e68cd0974268656fd4a1235
petr-tik <tikilyaynen@gmail.com>
2016-01-30 23:52:06 +0000

    solved unknown in range with O(2n).
    wrote up first solution, where a range of values is created given lower and upper bounds. Then a random element is deleted. To find the missing element,subtract every element in the range from the know or calculated sum of original range

--- unknown_in_range.py
+++ unknown_in_range.py
@@ -1,35 +1,53 @@
 """
 a Q from David Beck
 Given an unsorted sequence of numbers in the range between x and y (eg 1..100) with just one number missing. 
-Find a O(k) algorithm to calculate the missing value
+Find a linear algorithm to calculate the missing value
 """
+
+######################################################
+# solution 1
 
 import random
 
 class problem_range(object):
 	def __init__(self, lower_bound, upper_bound):
-		print "To make a range of numbers between 2 bounds, I need you to tell me the lower and upper bound"
-  	
+		
 		self.lower_bound = lower_bound
   		self.upper_bound = upper_bound
-  		if self.upper_bound <= self.lower_bound:
-  			print "The upper bound has to be greater than the lower bound"
-
-  		else: 
-  			full_range = list(xrange(self.lower_bound, self.upper_bound))
-	  		# delete random member
-  			del full_range[random.randint(self.lower_bound, (self.upper_bound - 1))]
-			print full_range
-
+  		self.full_range = list(xrange(self.lower_bound, self.upper_bound))
+	  	# delete random member
+  		del self.full_range[(random.randint(self.lower_bound, (self.upper_bound - 1))) - lower_bound]
 
 
 	def find_missing(self):
-		missing = 0
-		for x in xrange(self.lower_bound, self.upper_bound):
-			pass
+		missing = sum(xrange(self.lower_bound, self.upper_bound))
+		for x in self.full_range:
+			missing -= x
+
+		print "{} is missing from {}".format(missing, self.full_range)
   		
 
-lad = problem_range(3,5)
+lad = problem_range(3,10)
+
+lad.find_missing()
+
+
+######################################################
+# solution 2
+
+
+"""
+assign all value to hash table
+use the numbers in the range (number - 1)
+
+as keys to another list, where you can mark 
+
+[1,2,4,5,6] - 3 missing
+[1,1,1,0,1,1] - 
+
+
+
+"""
 
   
   
