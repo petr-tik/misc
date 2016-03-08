481b38b10d2300e5018cdce7f1bd19bbb28f80fc
petr-tik <tikilyaynen@gmail.com>
2016-03-08 19:12:33 +0000

    solution with dictionary, where alphabet is keys remove spaces and lowercase all chars in string, then step through it char by char and increment the according dict value. In the end check if any 0s in the dict.values() - not pangram, otherwise - pangram

---
+++ pangrams.py
@@ -0,0 +1,32 @@
+#! usr/bin/env/ python
+# https://www.hackerrank.com/challenges/pangrams
+import string
+
+alphabet = string.ascii_lowercase
+alph_dict = {}
+for char in alphabet:
+    alph_dict[char] = 0
+
+
+def is_pangram(s):
+    # given a string, return True if it's a pangram, otherwise, return False
+    # make a dict with letters as keys, if char found in string increment the value. 
+    # At the end of string, if dict has 0 value anywhere, it's not a pangram
+    # remove all spacing 
+    s = s.replace(" ", "")
+    
+    # step through the string (lowercased)
+    for char in s.lower():
+        # increment the value on the particular key
+        alph_dict[char] += 1
+
+    # if there is at least one 0 in list of values - at least one letter never appeared in the string    
+    if 0 in alph_dict.values():
+        print "not pangram"
+    else:
+        print "pangram"
+    
+
+is_pangram("hello how are you you beautiful bastard who lives in china before america takes over")
+    
+is_pangram("The quick brown fox jumps over the lazy dog")
