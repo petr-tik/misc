5321b47bab5789b9225948e14b8a9e6350601ead
petr-tik <tikilyaynen@gmail.com>
2016-03-10 08:55:31 +0000

    regexp solution with a is_email func and filter to go through the list of email candidates

---
+++ email_validator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import re
+
+
+pattern = re.compile("([a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-z]{1,3})")
+
+num_emails = int(raw_input("how many emails?\n"))
+
+emails_cands = []
+for _ in xrange(num_emails):
+    emails_cands.append(raw_input("Input email:\n"))
+
+def is_email(email):
+    if pattern.match(email):
+        return True
+    else:
+        return False
+
+emails = filter(is_email, emails_cands)
+
+print emails
