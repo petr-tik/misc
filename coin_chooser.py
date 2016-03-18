ce4682a0a56d1296d4d4ffb45146bc49700586bb
petr-tik <tikilyaynen@gmail.com>
2016-03-18 21:37:48 +0000

    speed the solution up by only considering coin values that are greater than the last added value

--- coin_chooser.py
+++ coin_chooser.py
@@ -3,29 +3,23 @@
 # https://www.hackerrank.com/contests/zenhacks/challenges/candy-shop
 
 import sys
-from sets import Set
 
 N = int(raw_input().strip())
 coins = [1, 2, 5, 10, 20, 50, 100]
 distr = []
-final_solution = Set()
+res = 0
 
 def solve(NUMBER):
     if NUMBER == 0:
-        distr_copy = [x for x in distr]
-        distr_copy.sort()
-        res = ""
-        for x in distr_copy:
-            res += str(x)
-        final_solution.add(res)
+        global res 
+        res += 1
         return
 
     for x in coins:
-        if x <= NUMBER:
+        if x <= NUMBER and (not distr or x >= distr[-1]):
             distr.append(x)
             solve(NUMBER - x)
             distr.pop()
-    
-    
+
 solve(N)
-print len(final_solution)
+print res
