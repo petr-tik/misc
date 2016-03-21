2d8ca74138027aa7b346e6d2aaa2491f1097372a
petr-tik <tikilyaynen@gmail.com>
2016-03-21 11:54:05 +0000

    HR solved. count the number of sticks and return a list of lengths, which is printed out with a for loop

---
+++ cut_sticks.py
@@ -0,0 +1,34 @@
+#! usr/bin/env python
+
+# https://www.hackerrank.com/challenges/cut-the-sticks
+
+"""
+Input Format 
+The first line contains a single integer NN. 
+The next line contains NN integers: a0, a1,...aN-1 separated by space, where ai represents the length of ith stick.
+
+Output Format 
+For each operation, print the number of sticks that are cut, on separate lines.
+
+"""
+
+
+def cutting(sticks):
+    # given a list of stick lengths print out how many sticks were cut
+    lengths = []
+    lengths.append(len(sticks))
+    while sticks: 
+        cut_measure = min(sticks)
+        print "new minimum: {}".format(cut_measure)
+        sticks = [x-cut_measure for x in sticks]
+        sticks = [x for x in sticks if x > 0]
+        if sticks:
+            lengths.append(len(sticks))
+    return lengths        
+
+
+y = cutting([5,4,5,2,3])
+for x in y:
+    print x
+
+
