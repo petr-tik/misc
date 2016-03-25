9fedce4ab008c3d6ae2faa11403395b0c8bc09e6
petr-tik <tikilyaynen@gmail.com>
2016-03-25 22:02:44 +0000

    added a func to generate random words to test

--- anagram_check.py
+++ anagram_check.py
@@ -2,13 +2,19 @@
 
 # take 2 words and check if they are anagrams
 import string
+import random
 
-word1 = raw_input()
-word2 = raw_input()
 
+def make_words():
+    # makes two random words
+    alphabet = string.lowercase
+    word1 = ''.join([alphabet[random.randint(0,25)] for x in xrange(random.randint(1,15))])
+    word2 = ''.join([alphabet[random.randint(0,25)] for x in xrange(random.randint(1,15))])
+    return word1, word2
 
 
 def is_anagram(first_word, second_word):
+    print "Is {} an anagram of {}?".format(first_word, second_word)
     alphabet = string.lowercase
     word1 = {x : 0 for x in alphabet}
     word2 = {x : 0 for x in alphabet}
@@ -28,5 +34,7 @@ def is_anagram(first_word, second_word):
     else:
         return True
 
-x = is_anagram(word1, word2)
-print x
+
+for x in xrange(5):
+    word1, word2 = make_words()
+    print is_anagram(word1, word2)
