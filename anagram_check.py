4935d5d9c50d942a91052ad4579283009ce40bb3
petr-tik <tikilyaynen@gmail.com>
2016-03-25 21:38:37 +0000

    try and except solution, which returns false in case of IndexError

---
+++ anagram_check.py
@@ -0,0 +1,32 @@
+#! usr/bin/env python
+
+# take 2 words and check if they are anagrams
+import string
+
+word1 = raw_input()
+word2 = raw_input()
+
+
+
+def is_anagram(first_word, second_word):
+    alphabet = string.lowercase
+    word1 = {x : 0 for x in alphabet}
+    word2 = {x : 0 for x in alphabet}
+    for letter in first_word:
+        for letter2 in second_word: 
+            try:
+                word1[letter] += 1
+                word2[letter2] += 1
+            except IndexError:
+                return False
+                
+    print word1
+    print word2
+
+    if word1 != word2:
+        return False
+    else:
+        return True
+
+x = is_anagram(word1, word2)
+print x
