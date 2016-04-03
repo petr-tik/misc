d00806eea8756810cd8bd9cab9ccb54e5e0f4565
petr-tik <tikilyaynen@gmail.com>
2016-04-03 22:18:09 +0100

    HR solved 100%. Used a helper function to make a list for the maximise profit function to look up the maximum value from idx to the end of datapoints original list. DP solution's cache. O(~2n) - linear solution

--- stock_maximiser.py
+++ stock_maximiser.py
@@ -11,25 +11,33 @@ for _ in xrange(T):
     datapoints = [int(x) for x in raw_input().split()]
 """
 
+def max_to_end(L):
+    temp_max = L[-1]
+    output_list = []
+    output_list.append(temp_max)
+    for idx in xrange(len(L) - 2, -1, -1):
+        if L[idx] > temp_max:
+            temp_max = L[idx]    
+        output_list.append(temp_max)
+    
+    return output_list[::-1]
 
 
-def maximise_profit(datapoints):
+def maximise_profit(datapoints, max_values):
     print "Starting with {}".format(datapoints)
     balance = 0
     shares_bought = 0
     for idx, price in enumerate(datapoints):
-        if price >= max(datapoints[idx:]):
+        if price >= max_values[idx]:
             balance += shares_bought*price
             print "Selling {} shares for {}".format(shares_bought, price)
             shares_bought = 0
-
         else:            
             balance -= price
             shares_bought += 1
             print "Buying one share for {}".format(price)
             
-
-        print "Given the array {} the profit is {}".format(datapoints[:idx+1], balance)
+        print "Given the array {} the balance is {}".format(datapoints[:idx+1], balance)
 
     print balance
 
@@ -37,4 +45,4 @@ def maximise_profit(datapoints):
 
 for x in xrange(5):
     datapoints = [random.randint(1,200) for x in xrange(10)]
-    maximise_profit(datapoints)
+    maximise_profit(datapoints, max_to_end(datapoints))
