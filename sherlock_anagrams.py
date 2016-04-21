7f2368ccd1ca9c9cd75ee5f4847b1c654769d12b
petr-tik <petr-tik.users.noreply.github.com>
2016-04-21 22:51:51 +0100

    started the sherlock anagrams problem

---
+++ sherlock_anagrams.py
@@ -0,0 +1,26 @@
+#! usr/bin/env/ python
+
+def make_dict(word):
+    "takes a string and returns a dict with character as key, count as value"
+    letters_in_words = {}
+    for char in word:
+        if char in letters_in_words:
+            letters_in_words[char] += 1
+        else:
+            letters_in_words[char] = 1
+
+    return letters_in_words
+
+
+
+def count_anagrams(phrase):
+    n = len(phrase)
+    for subarrays in xrange(2, n):
+        length = n - subarrays + 1
+
+
+
+
+
+
+
