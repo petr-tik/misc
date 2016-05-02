e6076a9264d56fa8c7e9031bb5bd0ac153a176b3
petr-tik <petr-tik@users.noreply.github.com>
2016-05-02 21:38:38 +0100

    2 loops done, need to sift through all combinations

--- sherlock_anagrams.py
+++ sherlock_anagrams.py
@@ -1,5 +1,7 @@
 #! usr/bin/env/ python
 
+from itertools import combinations
+
 # https://www.hackerrank.com/challenges/sherlock-and-anagrams
 
 def make_dict(word):
@@ -14,9 +16,9 @@ def make_dict(word):
     return letters_in_words
 
 
-def are_anagrams(string1, string2):
+def are_anagrams(tup):
     "takes 2 (sub)strings and checks if they are anagrams using the make_dict helper"
-    if make_dict(string1) == make_dict(string2):
+    if make_dict(tup[0]) == make_dict(tup[1]):
         return True
     else:
         return False 
@@ -27,28 +29,31 @@ def count_anagrams(phrase):
     n = len(phrase)
     # consider all substring lengths between 1 (one char) to (n - 1)
     answer = 0
-    for subarray_lengths in xrange(1, n): # will only consider non-empty strings 
-        length = n - subarray_lengths 
+    for subarray_length in xrange(n - 1, 0, -1): # will only consider non-empty strings 
         under_investigation = []
         # given the length, make a range of starting points, 
         # so you can make strings of that length from each start position
-        for start_pos in xrange(0, n - length + 1):
-            word = phrase[start_pos:start_pos+length]
+        for start_pos in xrange(0, n - subarray_length + 1):
+            # make a new word of new length from star_pos 
+            word = phrase[start_pos:start_pos+subarray_length]
+            # make a dict from that word
             dict_from_word = make_dict(word)
-            if answer == 0 and dict_from_word in under_investigation:
-                answer += 2
-            elif dict_from_word in under_investigation:
-                answer += 1
-            else:
-                under_investigation.append(dict_from_word)
+            under_investigation.append(dict_from_word)
+        print filter(are_anagrams, list(combinations(under_investigation, 2)))
+        print len(filter(are_anagrams, list(combinations(under_investigation, 2))))
     print answer 
 
 
 count_anagrams("abba")
+print "should be", 3
 count_anagrams("ifailuhkqq")
+print "should be", 2
 count_anagrams("hucpoltgty")
+print "should be", 2
 count_anagrams("ovarjsnrbf")
+print "should be", 6
 count_anagrams("pvmupwjjjf")
+print "should be", 3
 count_anagrams("iwwhrlkpek")
 
 """
