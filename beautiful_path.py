72dc1de1a66f4f0c0329f3d976fe6cd1f404c2a8
petr-tik <petr-tik@users.noreply.github.com>
2016-05-08 22:37:38 +0100

    hackerrank solved 100%

--- beautiful_path.py
+++ beautiful_path.py
@@ -6,54 +6,43 @@ from collections import deque
 
 # Turn the graph into unweighted, so you can run bfs on it. 
 # There are only 2**10 binary lengths between 2 vertices
-# Turn 
-#
-#
-#
 
 def make_graph():
     N, M = [int(x) for x in raw_input().split()]
-    graph = {}
+    graph = {x:[] for x in xrange(N)}    
     for _ in xrange(M):
         start, finish, cost = [int(x) for x in raw_input().split()]
-        if not graph.has_key(start):
-            graph[start] = [(finish, cost)]
-        else:
-            graph[start].append((finish, cost))
+        graph[start-1].append((finish-1, cost))
+        graph[finish-1].append((start-1, cost))
+         
     # assertEqual(N, len(graph))
     return graph
 
 
-def bfs(graph, start):
-    dist = [-1 for _ in xrange(len(graph))]
-    dist[start] = 0
-    q = deque([]) # make a queue from deque object
-    q.appendleft(start)
-    while q:
-        v = q.pop() # study the first out element from queue
-        for idx in xrange(len(graph[v])): # all v's children indices
-            v2 = g[v][idx] # each child 
-            if dist[v2] == -1: # still hasn't been visited
-                dist[v2] = dist[v] + 1 # to get to the child we need one more step than to parent
-                q.appendleft(v2)
+def bfs(graph, start, finish):
+    visited = set()
+    visited.add((start-1, 0))
+    min_cost = 1025
+    queue = deque([]) # make a queue from deque object
+    queue.appendleft((start-1, 0))
+    while queue:
+        v = queue.pop() # study the first out element from queue
+        for idx in xrange(len(graph[v[0]])): # all v's children indices
+            v2 = graph[v[0]][idx][0] # each child 
+            cost_v2 = graph[v[0]][idx][1] # and their cost
+            v_next = (v2, v[1] | cost_v2) # next node to add to the queue
+            if v_next not in visited: # still hasn't been visited
+                visited.add(v_next) # to get to the child we need one more step than to parent
+                queue.appendleft(v_next)
+                if v2 == finish-1:
+                    min_cost = min(min_cost, v_next[1])
 
-
-
-def solve(graph, start, finish):
-    paths = find_paths(graph, start, finish)
-    if len(paths) == 0:
-        print "-1"
-    elif len(paths) == 1:
-        print bitwise_or(dec_to_binary(paths[0]))
+    if min_cost != 1025: 
+        return min_cost
     else:
-        print find_min_path(paths)
+        return "-1"
 
 
-print bitwise_or(dec_to_binary([5,4,1024]))
-
-# graph = make_graph()
-# start = raw_input()
-# 
-
-# print make_graph()
-solve(make_graph(), 1, 3)
+graph = make_graph()
+start, finish = map(int, raw_input().split())
+print bfs(graph, start, finish)
