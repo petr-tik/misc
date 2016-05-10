305b2f698692de8168a7f339d54730e7c9c2a16f
Petr Tikilyaynen <petr-tik@users.noreply.github.com>
2016-05-10 18:10:59 +0100

    HackerRank solved
    O(N) complexity, where N = len(string)

--- alternating_chars.py
+++ alternating_chars.py
@@ -3,32 +3,21 @@
 # https://www.hackerrank.com/challenges/alternating-characters
 
 
-def s_to_char_loc(s):
-    # given a string, return a dict with letters as keys and indices in the string as values
-    res = {}
-    for idx, char in enumerate(s):
-        if not res.has_key(char):
-            res[char] = [idx]
-        else:
-            res[char].append(idx)
-
-    return res
-
-
-def is_alternate(s):
-    # given a string return true if it has alternate chars, otherwise false
-    letter_dict = s_to_char_loc(s)
-    for key in letter_dict:
-        pass
-
 def solve(s):
-    letter_dict = s_to_char_loc(s)
-    deletions = 0
-    if len(letter_dict) == 1:
-        deletions = len(s) - 1
+	last = s[0]
+	deleted = 0
+	for idx in xrange(1, len(s)):
+		if s[idx] == last:
+			deleted += 1
+		else:
+			last = s[idx]
+	return deleted
 
 
-    return deletions    
+N = int(raw_input())
+for _ in xrange(N):
+    s = raw_input()
+    print solve(s)
 
 
 print solve("AAAAAA")
