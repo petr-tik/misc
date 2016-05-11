e884e4ded6389ff0a1b300650d968f590c75facc
petr-tik <petr-tik@users.noreply.github.com>
2016-05-11 22:02:49 +0100

    added comments

--- palindrome_index.py
+++ palindrome_index.py
@@ -32,18 +32,29 @@ def word_to_dict(s):
 print is_palindrome('aaa')
 
 def solve(s):
+# brute force solution times out, but narrowing the search space doesn't
     if is_palindrome(s):
         print "-1"
-    
-    word_dict = word_to_dict(s)
+# turn the string into a dict with chars as keys 
+# and values of indices in the original string
+# you don't want to remove a char which appears an even number of times
+# so make a candidate space of chars, which appear odd number of times 
+# it's enough of an improvement - HackerRank solved
+    else:
+        word_dict = word_to_dict(s)
+        candidates = []
+        for key in word_dict:
+            if len(word_dict[key]) % 2 != 0:
+                candidates.extend(word_dict[key])
+        for idx in candidates:
+            cand_word = ''.join([s[0:idx], s[idx+1::]])
+            if is_palindrome(cand_word):
+                print idx
+                break
 
-    candidates = []
-    for key in word_dict:
-        if len(word_dict[key]) % 2 != 1:
-            candidates.extend(word_dict[key])
 
-    for idx in candidates:
-        if is_palindrome(''.join([s[0:idx], s[idx+1::]])):
-            print idx
-            break
+## HackerRank IO boilerplate
+N = int(raw_input())
+for _ in xrange(N):
+    solve(raw_input())
 
