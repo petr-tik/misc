4c35fefede843d15f99c93a587eca0cc983c6987
petr-tik <petr-tik@users.noreply.github.com>
2016-05-13 21:51:10 +0100

    minor improvement, still timing out

--- and_product.py
+++ and_product.py
@@ -3,8 +3,10 @@
 # https://www.hackerrank.com/challenges/and-product
 
 def solve(start, finish):
-    arr = [x for x in xrange(start, finish + 1)]
-    return reduce(lambda x,y: x&y, arr)
+    res = start
+    for x in xrange(start + 1, finish + 1):
+        res = res & x
+    return res
 
 T = int(raw_input())
 for _ in xrange(T):
