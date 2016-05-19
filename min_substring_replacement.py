2c9a2982c80f87b07fe11f433cac694b913f1754
petr-tik <petr-tik@users.noreply.github.com>
2016-05-19 23:55:29 +0100

    recovered is_steady predicate func

--- min_substring_replacement.py
+++ min_substring_replacement.py
@@ -6,34 +6,43 @@
 # http://articles.leetcode.com/finding-minimum-window-in-s-which/
 # http://www.geeksforgeeks.org/find-the-smallest-window-in-a-string-containing-all-characters-of-another-string/
 
+the logic is:
+look at the BigString and find out how many 
+
+
+def is_steady(char_dict, length):      
+    # takes a dict of chars from string and returns True if already steady        
+    if any(len(char_dict[key]) != length/4 for key in char_dict):     
+        return False      
+    return True
 
 def string_to_dict(string, length):
     substring_length = 0
     char_dict = {'A': [], 'G': [], 'C': [], 'T':[]}
-    too_many = {}
+    to_find = {}
     for idx, char in enumerate(string):
         char_dict[char].append(idx)
     
     for key in char_dict:
         difference = len(char_dict[key]) - length/4
         if difference > 0:
-            too_many[key] = difference
+            to_find[key] = difference
             substring_length += difference
 
-    # return char_dict, too_many, substring_length
-    return char_dict, too_many, length
+    return char_dict, to_find, length
 
+    
 
 def solve(string, length):
     # given a string, start looking for relevant substrings, 
     # as soon as you find one, output its length
-    char_dict, too_many, substring_length = string_to_dict(string, length)
+    char_dict, to_find, substring_length = string_to_dict(string, length)
     if is_steady(char_dict):
         return 0
     while substring_length < length:
         for start_pos in xrange(0, length - substring_length + 1):
             test_string = string[start_pos:start_pos + substring_length]
-            if all(test_string.count(x) == too_many[x] for x in too_many):
+            if all(test_string.count(x) == to_find[x] for x in to_find):
                 return substring_length
                 break
         substring_length += 1
