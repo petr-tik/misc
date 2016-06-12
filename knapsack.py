3d6086df6c2497782c825e05c184fcbf5120481d
petr-tik <petr-tik@users.noreply.github.com>
2016-06-12 18:42:42 +0100

    added memoization - now dynamically solved.
    test case 10, now runs in 0.08s instead of 2.51s
    need to pass a clean dict each time

--- knapsack.py
+++ knapsack.py
@@ -2,8 +2,9 @@
 
 # https://www.hackerrank.com/challenges/unbounded-knapsack
 
-def recur_solve(cur_value, arr, k):
-    considered = dict()
+def recur_solve(cur_value, arr, k, considered):
+    if cur_value in considered:
+        return considered[cur_value]
     
     if 1 in arr or any(k % item == 0 for item in arr):
         return k
@@ -14,9 +15,16 @@ def recur_solve(cur_value, arr, k):
     temp = cur_value
     for item in arr:
         if cur_value + item <= k:
-            res = recur_solve(cur_value + item, arr, k)
+            res = recur_solve(cur_value + item, arr, k, considered)
             temp = max(temp, res)
-            considered.a
-    return temp 
-
-print recur_solve(0, [3,6,9], 13)
+    
+    considered[cur_value] = temp    
+    return temp    
+    
+## HackerRank boilerplate    
+T = int(raw_input())
+for _ in xrange(T):
+    n, k = map(int, raw_input().split())
+    arr = map(int, raw_input().split())
+    considered = dict()
+    print recur_solve(0, arr, k, considered)
