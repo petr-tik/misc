ca3455bfd8ade4a71fbc7394f460aa938be7d385
petr-tik <petr-tik@users.noreply.github.com>
2016-06-13 09:02:23 +0100

    added comments

--- knapsack.py
+++ knapsack.py
@@ -2,10 +2,15 @@
 
 # https://www.hackerrank.com/challenges/unbounded-knapsack
 
+considered = {}
 def recur_solve(cur_value, arr, k, considered):
+    # memoization - if already solved, use the solution
     if cur_value in considered:
         return considered[cur_value]
     
+    # optimisation hacks
+    # you can use the numbers as many times as possible, so once you have:
+    # 1 or a multiple of k, you are good
     if 1 in arr or any(k % item == 0 for item in arr):
         return k
 
@@ -13,18 +18,24 @@ def recur_solve(cur_value, arr, k, considered):
         return cur_value
 
     temp = cur_value
+    # for the value already calculated, try adding every number from array
     for item in arr:
-        if cur_value + item <= k:
+        if cur_value + item <= k: # once the running total is greater than k, stop
             res = recur_solve(cur_value + item, arr, k, considered)
+            # the maximum you can get starting at cur_value is 
             temp = max(temp, res)
     
+    # now that temp is the maximum it can be, add it to a hashmap    
     considered[cur_value] = temp    
     return temp    
     
-## HackerRank boilerplate    
+## HackerRank boilerplate
+"""    
 T = int(raw_input())
 for _ in xrange(T):
     n, k = map(int, raw_input().split())
     arr = map(int, raw_input().split())
     considered = dict()
-    print recur_solve(0, arr, k, considered)
+    print recur_solve(0, arr, k, considered)"""
+
+print recur_solve(0, [4, 8, 12, 16], 5, considered)
