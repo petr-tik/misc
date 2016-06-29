c94d0611909d37e40cc0e1e6274c91fbd8044d0a
petr-tik <petr-tik@users.noreply.github.com>
2016-06-29 17:54:14 +0100

    moved comments, added input request

--- pi_estimate.py
+++ pi_estimate.py
@@ -12,12 +12,7 @@ def xy_gen():
 def solve(N):
     """Given a number N, run N,generators of x,y coordinates, 
     plot them on the graph and and count how many are inside the circle"""
-    inside = 0
-    for _ in xrange(N):
-        x, y = xy_gen()
-        if sqrt(x**2 + y**2) <= 1.0:
-            inside += 1
-"""
+    """
     Area of the square = 1 
     area of the 1/4 square = pi*r^2/4
     when we generate a random pair of coordinates, 
@@ -29,6 +24,13 @@ def solve(N):
     divide it by the number of points and multiply by 4
     
     return 4*(inside/float(N))
-"""
+    """
+    inside = 0
+    for _ in xrange(N):
+        x, y = xy_gen()
+        if sqrt(x**2 + y**2) <= 1.0:
+            inside += 1
+    return 4*(inside/float(N))
 
-print solve(50)
+N = int(raw_input("How many experiments do you want to try?\n"))
+print solve(N)
