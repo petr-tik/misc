a13abe37b2fb9bf20d4079866dfeeab1c5ba9556
petr-tik <petr-tik@users.noreply.github.com>
2016-07-04 08:10:03 +0100

    solution works locally

--- min_substring_replacement.py
+++ min_substring_replacement.py
@@ -6,9 +6,10 @@
 # http://articles.leetcode.com/finding-minimum-window-in-s-which/
 # http://www.geeksforgeeks.org/find-the-smallest-window-in-a-string-containing-all-characters-of-another-string/
 
-the logic is:
-look at the BigString and find out how many 
-
+# the logic is:
+# look at the BigString and find out how many chars are missing. Make a hashmap with count for its missing character.
+# go through the string again with begin and start pointers. When you find all the missing chars, mark the end pointer to the last char and record the overall length of the window 
+# try to move the start pointer as far left as possible. Go through the rest of BigString looking to move the start pointer as far right as possible. 
 
 def is_steady(char_dict, length):      
     # takes a dict of chars from string and returns True if already steady        
@@ -16,39 +17,68 @@ def is_steady(char_dict, length):
         return False      
     return True
 
-def string_to_dict(string, length):
-    substring_length = 0
-    char_dict = {'A': [], 'G': [], 'C': [], 'T':[]}
-    to_find = {}
-    for idx, char in enumerate(string):
-        char_dict[char].append(idx)
+def analyse(string, length):
+    abs_min_length = 0
+    char_dict = {'A': 0, 'G': 0, 'C': 0, 'T':0}
     
+    # makes a counter of times each character appears
+    for idx, char in enumerate(string):
+        char_dict[char] += 1
+    
+    # makes a to_find dictionary with the counter 
+    # for how many times we need each char in a substring and its min length
+    need_to_find = {}
     for key in char_dict:
-        difference = len(char_dict[key]) - length/4
+        difference = char_dict[key] - length/4
         if difference > 0:
-            to_find[key] = difference
-            substring_length += difference
+            need_to_find[key] = difference
+            # absolute minimal length of substring
+            abs_min_length += difference
+        else:
+            need_to_find[key] = 0
 
-    return char_dict, to_find, length
+    return char_dict, need_to_find, abs_min_length
 
     
 
 def solve(string, length):
     # given a string, start looking for relevant substrings, 
     # as soon as you find one, output its length
-    char_dict, to_find, substring_length = string_to_dict(string, length)
-    if is_steady(char_dict):
-        return 0
-    while substring_length < length:
-        for start_pos in xrange(0, length - substring_length + 1):
-            test_string = string[start_pos:start_pos + substring_length]
-            if all(test_string.count(x) == to_find[x] for x in to_find):
-                return substring_length
-                break
-        substring_length += 1
-    return length
+    char_dict, need_to_find, abs_min_length = analyse(string, length)
+    
+    has_found = {'A': 0, 'G': 0, 'C': 0, 'T':0}
+    min_substring_length = length + 1
+    counter = 0
+    start_pointer = 0
+    for end_pointer in xrange(length):
+        end_char = string[end_pointer]
+        if need_to_find[end_char] == 0:
+            continue # if we don't need it - keep advancing the end pointer
+        # otherwise, do as below
+        has_found[end_char] += 1
+        
+        if has_found[end_char] <= need_to_find[end_char]:
+            counter += 1 
+        if counter == abs_min_length: 
+            # advance begin index as far right as possible,
+            # stop when advancing breaks window constraint.
+            while need_to_find[string[start_pointer]] == 0 or has_found[string[start_pointer]] > need_to_find[string[start_pointer]]:
+                slide_char = string[start_pointer]
+                if (has_found[slide_char] > need_to_find[slide_char]):
+                    has_found[slide_char] -= 1
+                start_pointer += 1
+          
+            window = end_pointer - start_pointer + 1
+            if window < min_substring_length:
+                min_substring_length = window
+
+    return min_substring_length 
+
+
                 
-print string_to_dict('TGATGCCGTCCCCTCAACTTGAGTGCTCCTAATGCGTTGC', 40)
+print solve('TGATGCCGTCCCCTCAACTTGAGTGCTCCTAATGCGTTGC', 40)
+print solve('TTTT', 4)
+
 
 # HackerRank boilerplate
 """length = int(raw_input())
