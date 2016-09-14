bf91e066359bc6e04c4d0c06b80959e94bad815e
petr-tik <petr-tik@users.noreply.github.com>
2016-09-14 20:11:11 +0100

    fib_to_dec converter - done

--- fib_base.py
+++ fib_base.py
@@ -1,12 +1,15 @@
 #! /usr/bin/env python
 
-# https://www.reddit.com/r/dailyprogrammer/comments/5196fi/20160905_challenge_282_easy_unusual_bases/
 
-""" Fibbonacci base is when instead of binary the 1 or 0 shows how many times you take the fib number of this index
+"""
+
+https://www.reddit.com/r/dailyprogrammer/comments/5196fi/20160905_challenge_282_easy_unusual_bases/
+
+Fibbonacci base is when instead of binary the 1 or 0 shows how many times you take the fib number of this index
 
 dec 8 = 1 0 1 1 0 in fib
 
-Write a converter that takes base (dec or fib) and returns the 
+Write a converter that takes base (dec or fib) and returns the number in the other base
 
 """
 
@@ -19,14 +22,22 @@ def fib(n):
     while counter < n:
         ret.append(ret[-1] + ret[-2])
         counter += 1
-    return ret
+    return ret[::-1]
 
 def dec_to_fib(num):
     pass
 
 
 def fib_to_dec(num):
-    pass
+    """Takes a string in fibonacci base in form 100010 and converts it to decimal value """
+    fib_seq = fib(len(num))
+    res = 0
+    for idx, item in enumerate(num):
+        print int(item), fib_seq[idx]
+        res += int(item) * fib_seq[idx]
+
+    return res
+
 
 def convert(num, base_in, base_out):
     """ Given a number and its base, return the number in base_out """
@@ -40,3 +51,4 @@ def solve():
 
 print fib(2)
 print fib(8)
+print fib_to_dec("1000000000")
