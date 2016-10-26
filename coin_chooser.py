7dfe0cf9bebd4ec0ebe6a38bbb2545ef7b9e8f80
petr-tik <petr-tik@users.noreply.github.com>
2016-10-26 22:40:01 +0100

    Put everything into solve method
    internalised cache creation, coin values and function definition

--- coin_chooser.py
+++ coin_chooser.py
@@ -4,29 +4,39 @@
 
 import sys
 
-N = int(raw_input().strip())
-coins = [1, 2, 5, 10, 20, 50, 100]
-distr = []
-cache = [[-1 for _ in coins] for _ in xrange(N + 1)] 
 
-def solve(NUMBER, start_pos):
-    # check if the value has been calculated
-    if cache[NUMBER][start_pos] != -1:
-        return cache[NUMBER][start_pos]
-    
-    res = 0
+def solve():
+    """ Requests input, creates a cache array and returns the answer,
+    using the method defined internally """
+    value = int(raw_input().strip())
+    coin_values = [1, 2, 5, 10, 20, 50, 100]
+    cache = [[-1 for _ in coin_values] for _ in xrange(value + 1)]
 
-    if NUMBER == 0:
-        res = 1
+    def calc_ways_to_make_number_from_coins(NUMBER, start_coin=0):
+        """ Given a value and a start coin value index (default = 0)
+        return the number of ways to make value
+        by combining an infinite number of coins of given values
+        """
+        # check if the value has been calculated
+        if cache[NUMBER][start_coin] != -1:
+            return cache[NUMBER][start_coin]
 
-    else:
-        for i in xrange(start_pos, len(coins)):
-            if coins[i] <= NUMBER:
-                res += solve(NUMBER - coins[i], i)
-            else:
-                break
-    
-    cache[NUMBER][start_pos] = res
-    return res
+        res = 0
 
-print solve(250, 0)
+        if NUMBER == 0:
+            res = 1
+
+        else:
+            for i in xrange(start_coin, len(coin_values)):
+                if coin_values[i] <= NUMBER:
+                    res += calc_ways_to_make_number_from_coins(
+                        NUMBER - coin_values[i], i)
+                else:
+                    break
+
+        cache[NUMBER][start_coin] = res
+        return res
+
+    return calc_ways_to_make_number_from_coins(value)
+
+print solve()
