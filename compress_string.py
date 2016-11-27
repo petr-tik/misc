12491bd8c2b6f21dd991fb57cd66d4ee0c663166
petr-tik <petr-tik@users.noreply.github.com>
2016-11-27 21:31:25 +0000

    functional solution and pytests

--- compress_string.py
+++ compress_string.py
@@ -4,7 +4,7 @@ import pytest
 
 """ Write a function that compresses a string and returns it """
 
-def compress(input_string):
+def iter_compress(input_string):
     """ Given a string like aaabbcccdff, return a3b2c3d1f2 """
     res = []
     for char in input_string:
@@ -19,10 +19,28 @@ def compress(input_string):
     return "".join(res)
 
 
-def test_compress():
-    assert compress("aaabbcccdff") == "a3b2c3d1f2"
+def func_compress(input_string, res=[]):
+    if input_string == "":
+        outcome = format("".join(res))
+        return outcome
+    new_char = input_string[0]
+
+    if not res or new_char != res[-1][0]:
+        res.append("{}1".format(new_char))
+    else:
+        count_of_last_char = int(res[-1][1])
+        new_char_n_count = "{}{}".format(new_char, count_of_last_char + 1)
+        res[-1] = new_char_n_count
+
+    return func_compress(input_string[1:], res)
+
+def test_iter_compress():
+    assert iter_compress("aaabbcccdff") == "a3b2c3d1f2"
+
+def test_iter_is_func():
+    x = "aaabbcccdff"
+    assert iter_compress(x) == func_compress(x)
 
 if __name__ == '__main__':
-    print compress("aaabbcccdff")
-    print compress("aaabbcccdff") == "a3b2c3d1f2"
-    #test_compress()
+    test iter_compress()
+    test_iter_is_func()
