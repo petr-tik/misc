586966cb7d80b0666d58144bc43d713e5faf02a0
petr-tik <petr-tik@users.noreply.github.com>
2017-01-18 23:38:08 +0000

    added skeletons for methods

--- colour_adjacent.py
+++ colour_adjacent.py
@@ -11,6 +11,18 @@ Recursive bfs with starting (x, y) as start and all its neighbours as children. 
 """
 
 
+class Node():
+    """ Node for 2D matrix representation, where nodes 
+    that share an edge are neighbours """
+
+    def __init__(self, value, child1, child2, child3, child4):
+        self.value = value
+        self.child1 = child1
+        self.child2 = child2
+        self.child3 = child3
+        self.child4 = child4
+
+
 def matrix_index(matrix, index_tuple):
     """ Input:
                 a matrix (array of arrays)
@@ -28,3 +40,14 @@ def matrix_index(matrix, index_tuple):
 test_matrix = [[0 for _ in range(3)], [1 for _ in range(3)], [
     2 for _ in range(3)], [3 for _ in range(3)]]
 print(matrix_index(test_matrix, (1, 2)))
+def colour_matrix(matrix, coords, new_colour):
+    """ Input: 
+    a matrix of colour-values, 
+    new colour value 
+    and a tuple of coordinates, 
+    colour all pixels neighbouring the tuple of coordinates into the new colour
+
+    Output:
+    a matrix with new colours
+    """
+    pass
