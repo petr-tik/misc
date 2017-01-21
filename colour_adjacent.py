26b5151e104261993d2f14ad4642a659d6806539
petr-tik <petr-tik@users.noreply.github.com>
2017-01-21 12:21:23 +0000

    Add main method and helpers
    2 helper methods:
      find_neighbour_coords - returns an array tuples of all neighbouring
      coordinates
      find_same_colour_adjacent - uses the one above to return an array of
      coordinates, which exist in the matrix (not out of bounds) and have
      the same colour as input coord

--- colour_adjacent.py
+++ colour_adjacent.py
@@ -39,9 +39,58 @@ def setAtIndex(matrix, index_tuple, new_val):
         matrix[index_tuple[1]][index_tuple[0]] = new_val
     except IndexError:
         raise IndexError
+
+
+def find_neighbour_coords(coord):
+    right = (coord[0], coord[1] + 1)
+    left = (coord[0], coord[1] - 1)
+    up = (coord[0] + 1, coord[1])
+    down = (coord[0] - 1, coord[1])
+    return [right, left, up, down]
+
+
+def find_same_colour_adjacent(matrix, coord):
+    """ Given a matrix and a tuple of coordinates,
+    return an array of coordinate tuples of pixels with the same colour as coord"""
+    coordinates = []
+    colour = getAtIndex(matrix, coord)  # current target colour
+    cand_coords = find_neighbour_coords(coord)
+    for coord in cand_coords:
+        try:
+            next_colour = getAtIndex(matrix, coord)
+            if next_colour == colour:
+                coordinates.append(coord)
+        except IndexError:
+            pass
+            # do nothing on IndexError - just ignore the coordinates out of
+            # bounds
+    return coordinates
+
+
+def colour_matrix(matrix, cur, new_colour, visited=set()):
+    """ Input:
+    a matrix of colour-values,
+    new colour value
+    and a tuple of coordinates,
     colour all pixels neighbouring the tuple of coordinates into the new colour
 
     Output:
     a matrix with new colours
     """
-    pass
+    visited.add(cur)
+    children = find_same_colour_adjacent(matrix, cur)
+    # print(children)
+    setAtIndex(matrix, cur, new_colour)
+    for child in children:
+        if child not in visited:
+            colour_matrix(matrix, child, new_colour, visited)
+
+    return matrix
+
+
+def solve():
+    test_m = [[0, 0, 5, 0], [5, 0, 5, 0], [5, 5, 5, 5], [0, 0, 0, 0]]
+    res = [[0, 0, 9, 0], [9, 0, 9, 0], [9, 9, 9, 9], [0, 0, 0, 0]]
+    print(res == colour_matrix(test_m, (0, 2), 9))
+
+solve()
