bc1e7ac262d0d1e7b7c9ab694576c759055fbff0
petr-tik <petr-tik@users.noreply.github.com>
2017-01-21 12:20:29 +0000

    Delete old ideas, add getters and setters

--- colour_adjacent.py
+++ colour_adjacent.py
@@ -11,40 +11,34 @@ Recursive bfs with starting (x, y) as start and all its neighbours as children. 
 """
 
 
-class Node():
-    """ Node for 2D matrix representation, where nodes 
-    that share an edge are neighbours """
-
-    def __init__(self, value, childUp=None, childDown=None, childLeft=None, childRight=None):
-        self.value = value
-        self.childUp = childUp
-        self.childDown = childDown
-        self.childLeft = childLeft
-        self.childRight = childRight
-
-
-def matrix_index(matrix, index_tuple):
+def getAtIndex(matrix, index_tuple):
     """ Input:
                 a matrix (array of arrays)
                 coordinates (tuple of 2 elements)
+                    tuple is of the form (y, x)
         Output:
                 value at given index, if present
                 IndexError, if absent
     """
     try:
-        return matrix[index_tuple[0]][index_tuple[1]]
+        res = matrix[index_tuple[1]][index_tuple[0]]
+        return res
     except IndexError:
         raise IndexError
 
 
-test_matrix = [[0 for _ in range(3)], [1 for _ in range(3)], [
-    2 for _ in range(3)], [3 for _ in range(3)]]
-print(matrix_index(test_matrix, (1, 2)))
-def colour_matrix(matrix, coords, new_colour):
-    """ Input: 
-    a matrix of colour-values, 
-    new colour value 
-    and a tuple of coordinates, 
+def setAtIndex(matrix, index_tuple, new_val):
+    """ Input:
+                a matrix (array of arrays)
+                coordinates (tuple of 2 elements)
+                    tuple is of the form (y, x)
+        Output:
+               nothing, changes value in matrix inplace
+    """
+    try:
+        matrix[index_tuple[1]][index_tuple[0]] = new_val
+    except IndexError:
+        raise IndexError
     colour all pixels neighbouring the tuple of coordinates into the new colour
 
     Output:
