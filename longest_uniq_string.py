e98cbea7fcde6cd308344d61274bab4862ad04c7
petr-tik <petr-tik@users.noreply.github.com>
2017-01-29 02:16:57 +0000

    Added a new puzzle and solution

---
+++ longest_uniq_string.py
@@ -0,0 +1,54 @@
+"""
+Find longest substring with unique characters in O(n) time.
+
+Bloomberg puzzle
+"""
+
+import string
+
+example1 = string.ascii_lowercase
+example2 = "bacabacaddolz"
+example3 = "aaaaaaaa"
+
+
+def longest_substr(big_string):
+    """ Input:
+            ascii string
+        Returns:
+            length
+            longest substring of unique characters
+    """
+    res = 1
+    if len(big_string) == 1:
+        return res
+    left_ptr = 0
+    right_ptr = 1
+    uniq_letters = list(big_string[left_ptr])
+    while right_ptr < len(big_string):
+        if big_string[right_ptr] not in uniq_letters:
+            uniq_letters.append(big_string[right_ptr])
+            res = max(res, right_ptr - left_ptr + 1)
+            right_ptr += 1
+            continue
+        elif big_string[left_ptr] == big_string[right_ptr]:
+            left_ptr = right_ptr
+            right_ptr = left_ptr + 1
+            try:
+                uniq_letters = list(big_string[left_ptr])
+                continue
+            except:
+                break
+        else:  # char at right_ptr is already in set
+            left_ptr = right_ptr - 1
+            uniq_letters = list(big_string[left_ptr])
+
+    return "".join(uniq_letters)
+
+
+def test():
+    print(longest_substr(example1) == example1)
+    print(longest_substr(example2) == "dolz")
+    print(longest_substr(example3) == "a")
+
+
+test()
