0d9a465cbd828d0f355bc7bf0518ea957708d4c7
petr-tik <petr-tik@users.noreply.github.com>
2017-02-03 02:01:08 +0000

    Sketches of a solution - WIP

---
+++ check_bipartite_graph.py
@@ -0,0 +1,53 @@
+#! /usr/bin/env python2
+
+
+""" A bipartite graph is one, where each node belongs to either of 2 sets,
+such that no 2 nodes in the same set are adjacent.
+
+Given a graph, return True if it's bipartite, False otherwise """
+
+import collections
+
+test_bipartite = {1: [4, 5, 6], 2: [4, 5, 6], 3: [4, 5, 6],
+                  4: [1, 2, 3], 5: [1, 2, 3], 6: [1, 2, 3]}
+test_not_bipartite = {1: [4, 5, 6], 2: [4, 5, 6], 3: [4, 5, 6],
+                      4: [1, 2, 3], 5: [1, 2, 6], 6: [1, 2, 3]}
+
+
+def is_bipartite(graph, start=1):
+
+    queue = collections.deque([start])
+    return bfs(graph, queue)
+
+
+def bfs(graph, queue, visited=set(), counter=0, set_a=[], set_b=[]):
+    """ Given a graph and a starting point,
+    return True if bipartite, false otherwise"""
+    while queue:
+        cur = queue.popleft()
+        visited.add(cur)
+        children = graph[cur]
+        eligible_children = [
+            x for x in children if x not in queue and x not in visited]
+        if counter % 2 == 0:
+            if any(child in set_a for child in children):
+                return False
+            else:
+                set_b.extend(eligible_children)
+                queue.extend(eligible_children)
+                bfs(graph, queue, visited, counter + 1, set_a, set_b)
+        elif counter % 2 == 1:
+            if any(child in set_b for child in children):
+                return False
+            else:
+                set_a.extend(eligible_children)
+                queue.extend(eligible_children)
+                bfs(graph, queue, visited, counter + 1, set_a, set_b)
+    return True
+
+
+def check():
+    print(is_bipartite(test_bipartite, 2) == True)
+    print(is_bipartite(test_not_bipartite, 3) == False)
+
+check()
