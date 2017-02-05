6d4c2ba006241e4cf12079d3a1bd91618e7ca9d4
petr-tik <petr-tik@users.noreply.github.com>
2017-02-05 21:06:21 +0000

    Improved the non-bipartite test case
    More debugging

--- check_bipartite_graph.py
+++ check_bipartite_graph.py
@@ -4,7 +4,7 @@
 """ A bipartite graph is one, where each node belongs to either of 2 sets,
 such that no 2 nodes in the same set are adjacent.
 
-Given a graph, return True if it's bipartite, False otherwise 
+Given a graph, return True if it's bipartite, False otherwise
 
 Graphs are represented as adjacency dictionaries with digits for nodes
 
@@ -14,8 +14,11 @@ import collections
 
 test_bipartite = {1: [4, 5, 6], 2: [4, 5, 6], 3: [4, 5, 6],
                   4: [1, 2, 3], 5: [1, 2, 3], 6: [1, 2, 3]}
-test_not_bipartite = {1: [4, 5, 6], 2: [4, 5, 6], 3: [4, 5, 6],
-                      4: [1, 2, 3], 5: [1, 2, 6], 6: [1, 5, 3]}
+
+test_not_bipartite = {digit: list(range(7)) for digit in range(7)}
+
+# 1: [4, 5, 6], 2: [4, 5, 6], 3: [4, 5, 6],
+#                       4: [1, 2, 3], 5: [1, 2, 6], 6: [1, 5, 3]}
 
 
 def is_bipartite(graph, start=1):
@@ -31,6 +34,7 @@ def bfs(graph, queue, visited=set(), counter=0, set_a=[], set_b=[]):
         cur = queue.popleft()
         if cur == None:
             counter += 1
+            return bfs(graph, queue, visited, counter, set_a, set_b)
         visited.add(cur)
         children = graph[cur]
         eligible_children = [
@@ -43,19 +47,20 @@ def bfs(graph, queue, visited=set(), counter=0, set_a=[], set_b=[]):
                 set_b.extend(eligible_children)
                 queue.extend(eligible_children)
                 queue.append(None)
-                bfs(graph, queue, visited, counter + 1, set_a, set_b)
+                return bfs(graph, queue, visited, counter, set_a, set_b)
         elif counter % 2 == 1:
             if any(child in set_b for child in children):
+                print cur, children, set_a, counter
                 return False
             else:
                 set_a.extend(eligible_children)
                 queue.extend(eligible_children)
-                bfs(graph, queue, visited, counter + 1, set_a, set_b)
+                return bfs(graph, queue, visited, counter, set_a, set_b)
     return True
 
 
 def check():
-    print is_bipartite(test_bipartite, 2)
-    print is_bipartite(test_not_bipartite, 3)
+    print(is_bipartite(test_bipartite, 1))
+    print(is_bipartite(test_not_bipartite, 4))
 
 check()
