74dc28e77a46f3182df33e019f3791dc856ea591
petr-tik <petr-tik@users.noreply.github.com>
2017-02-16 00:49:00 +0000

    New puzzle

---
+++ sort_numbers_lexicographically.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3
+
+"""
+ co: Bloomberg
+ given a number N, print numbers from 1 to N in lexicographic order
+e.g., 1 -> 25: 1,10,11,12,....,19,2,20,21,...,25,3,4,5,6,7,8,9
+e.g   1 -> 247 1, 10, 100,
+
+
+"""
