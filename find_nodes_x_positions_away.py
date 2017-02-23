73415fd99fa70f923b68ed595dd098132b083bd0
petr-tik <petr-tik@users.noreply.github.com>
2017-02-23 19:21:26 +0000

    Solved it with recursive dfs

--- find_nodes_x_positions_away.py
+++ find_nodes_x_positions_away.py
@@ -1,6 +1,6 @@
 """
 co: gsa
-status: WIP
+status: done
 
 Given a tree, target Node and an int value X, 
 return all nodes X edges away from the target node
@@ -9,10 +9,12 @@ A graph is represented as an adjacency hashmap.
 {a: [b,c], b: [d, e], c:[f, g], d:[h]}
 """
 
-z_in_graph = {"a": ["b", "c"], "b": ["d", "e"],
-              "c": ["f", "g"], "d": ["h", "z"], "e": [], "f": [], "g": [], "h": [], "z": []}
-z_not_in_graph = {"a": ["b", "c"], "b": ["d", "e"],
-                  "c": ["f", "g"], "d": ["h", "z"], "f": [], "g": [], "h": []}
+z_in_graph = {"a": ["b", "c"], "b": ["a", "d", "e"],
+              "c": ["a", "f", "g"], "d": ["b", "h", "z"],
+              "e": ["b"], "f": ["c"], "g": ["c"], "h": ["d"], "z": ["d"]}
+z_not_in_graph = {"a": ["b", "c"], "b": ["a", "d", "e"],
+                  "c": ["a", "f", "g"], "d": ["b", "h"],
+                  "e": [], "f": ["c"], "g": ["c"], "h": []}
 
 
 def node_in_graph(graph, target_node, start=None, visited=set()):
@@ -31,9 +33,34 @@ def node_in_graph(graph, target_node, start=None, visited=set()):
                 return True
             if child not in visited:
                 stack.append(child)
-        print(stack, visited)
 
     return False
 
 print(node_in_graph(z_in_graph, "z", start="a"))
 print(node_in_graph(z_not_in_graph, "z", start="a"))
+
+
+def nodes_at_distance(graph, dist, start, res=list(), visited=set()):
+    """
+    Input:
+    - graph as an adjacency hashmap
+    - start - node to start with
+    - distance - integer how many edges away from the target_node
+
+    Return:
+    - array of vertices `dist` edges away from the start
+    (array may be empty)
+
+    Called recursively with a decreasing distance var. 
+    Nodes that you reach with distance of 0, are the ones you need.
+    """
+    visited.add(start)
+    if dist == 0 or:
+        res.append(start)
+        return res
+    for child in graph[start]:
+        if child not in visited:
+            res = nodes_at_distance(graph, dist - 1, child, res, visited)
+    return res
+
+print(nodes_at_distance(z_in_graph, 3, "z"))
