6731eb755e213d242b3ef157fa303d294a603ac8
petr-tik <petr-tik@users.noreply.github.com>
2017-03-04 22:48:46 +0000

    new puzzle

---
+++ func_in_binary_tree.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+
+
+"""
+co: gsa
+status: wip
+
+Given a binary tree, implement `f(root, v, dist)`, that returns all nodes in the tree `root`, `dist` steps away from `v`
+
+"""
