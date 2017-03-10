e32dce8a30f1a1f456a9e8086dc5470d95f4745e
petr-tik <petr-tik@users.noreply.github.com>
2017-03-10 14:12:26 +0000

    new puzzle

---
+++ split_array.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+
+"""
+co: gsa
+status: wip
+
+Given an array of ints, split it into k heaps, such that the difference between the heap with highest total sum and the heap with lowest total sum
+
+"""
